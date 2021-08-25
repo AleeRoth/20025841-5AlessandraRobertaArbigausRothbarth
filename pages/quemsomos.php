aff8da0f3cc0cdcbec537a5bd1349b7d19eb0a3d
AleeRoth <ale_rothbarth@hotmail.com>
2021-08-25 11:50:14 -0300

    correção de erros

--- pages/quemsomos.php
+++ pages/quemsomos.php
@@ -4,7 +4,7 @@
 
 
 
-<p> Meu Querido Bolo surgiu pelo amor a confeitaria e vontade de mostrar para todos as delicias que sempre comi na minha vida!
+<p> eu Querido Bolo surgiu pelo amor a confeitaria e vontade de mostrar para todos as delicias que sempre comi na minha vida!
 	Fundada em 2021, nossa missão é entregar produtos da melhor qualidade para trazer alegria na vida das pessoas.
 </p>
 <img src="./imagens/logotipo.jpg" alt="logo-doceria" style="width: 340px;">
